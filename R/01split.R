92401573209cc7f5831c1b4e97468a5e046492c7
Wu <s.wu@uu.nl>
2020-06-23 10:50:14 +0200

    update

--- R/01split.R
+++ R/01split.R
@@ -7,13 +7,9 @@
 #'
 #' @return a list
 redat<- function(svy.dat){
-  # remove strata column
   dat<- svy.dat[,-1]
-  # group respondent
   r<- dplyr::bind_cols(dat[seq(2,ncol(dat),2)])
-  # group sample
   n<- dplyr::bind_cols(dat[seq(1,ncol(dat),2)])
-  #
   res<- list(r=r,n=n)
   return(res)
 }
